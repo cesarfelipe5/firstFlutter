b34e233043f48a6ab370cb2ee8ef3d5bbc549151
cesarLaon <cesar@laon.com.br>
2021-08-30 10:11:09 -0300

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Center(
+          child: GestureDetector(
+        child: Text(
+          'Flutter: $counter',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
